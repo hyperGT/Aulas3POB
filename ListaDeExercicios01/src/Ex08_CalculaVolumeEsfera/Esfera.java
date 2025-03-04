42b674d7aee742af8328dcc2599d2b5a36f886a7
Gabriel Teixeira Charles da Silva <charlesgabriel102@gmail.com>
2025-03-04 15:49:28 -0300

    Programa que calcula o volume de uma esfera

---
+++ ListaDeExercicios01/src/Ex08_CalculaVolumeEsfera/Esfera.java
@@ -0,0 +1,24 @@
+package Ex08_CalculaVolumeEsfera;
+
+public class Esfera {
+   private double raio;
+      
+   public double volume(double radius){
+    return (4/3) * Math.PI * Math.pow(radius, 3);
+   }
+
+   public double volume(){
+    double radius = this.raio;
+    return (4/3) * Math.PI * Math.pow(radius, 3);
+   }
+
+   public static void main(String[] args) {
+        Esfera esfera = new Esfera();
+        
+        double rad = Double.parseDouble(System.console().readLine("Insira o raio: "));        
+
+        esfera.raio = rad;
+
+        System.out.printf("Volume da esfera: %.2f", esfera.volume());
+   }
+}
