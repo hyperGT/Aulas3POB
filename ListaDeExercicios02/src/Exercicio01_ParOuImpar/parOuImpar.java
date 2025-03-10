4b1f31c7424d4e414156d3e2396e7d64e72323fc
hyperGT <charlesgabriel102@gmail.com>
2025-03-10 11:33:01 -0300

    Novo exercicio: Par ou impar

--- ListaDeExercicios02/src/Exercicio01_ParOuImpar/parOuImpar.java
+++ ListaDeExercicios02/src/Exercicio01_ParOuImpar/parOuImpar.java
@@ -1,5 +1,20 @@
 package Exercicio01_ParOuImpar;
 
+import java.util.Scanner;
+
 public class parOuImpar {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Digite um número inteiro: ");
+
+        int numero = sc.nextInt();
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é par.");
+        } else {
+            System.out.println("O número " + numero + " é ímpar.");
+        }
+
+        sc.close();
+    }
 }
