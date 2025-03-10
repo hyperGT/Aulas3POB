7ccbdfcd12618d81150f18458505c0d69d51309a
hyperGT <charlesgabriel102@gmail.com>
2025-03-10 11:51:13 -0300

    Novo Exercicio: Conversão de notas para conceitos

---
+++ ListaDeExercicios02/src/Exercicio05_Notas/NotasParaConceito.java
@@ -0,0 +1,26 @@
+package Exercicio05_Notas;
+
+public class NotasParaConceito {
+    
+    public static void main(String[] args) {
+        
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        
+        System.out.print("Digite a nota: ");
+        int nota = sc.nextInt();
+        
+        if(nota >= 9) {
+            System.out.println("Conceito A");
+        } else if(nota >= 7) {
+            System.out.println("Conceito B");
+        } else if(nota >= 5) {
+            System.out.println("Conceito C");
+        } else if(nota >= 3) {
+            System.out.println("Conceito D");
+        } else {
+            System.out.println("Conceito E");
+        }
+        
+        sc.close();
+    }
+}
