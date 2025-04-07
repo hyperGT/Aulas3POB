e2732c7fe96877e97c750441c966e72eb0b16921
Gabriel <charlesgabriel102@gmail.com>
2025-04-07 11:14:52 -0300

    comentários adicionados

--- Exemplos/JException/src/NPointerExc/NPointerException.java
+++ Exemplos/JException/src/NPointerExc/NPointerException.java
@@ -1,5 +1,13 @@
 package NPointerExc;
 
+/*
+ * NPointerException.java
+ * This class demonstrates a NullPointerException in Java.
+ * It shows how to handle the exception using try-catch blocks.
+ * NullPointer exception occurs when an application attempts to use an object reference that has the null value.
+ * Ocorre em tempo de execução* Happens at runtime** 
+ */
+
 public class NPointerException {
     
     public static void main(String[] args) {
