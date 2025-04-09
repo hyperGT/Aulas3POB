9fe9ad069781d3e2292dfe38e2d2d47768a208ce
hyperGT <charlesgabriel102@gmail.com>
2025-04-09 10:43:44 -0300

    Exemplo de como criar e usar uma classe de exceção personalizada

---
+++ Exemplos/JException/src/Exceptions/Exemplo5.java
@@ -0,0 +1,21 @@
+class IdadeInvalidaException extends Exception {
+    public IdadeInvalidaException(String mensagem) {
+        super(mensagem);
+    }
+}
+
+public class Exemplo5 {
+    public static void verificarIdade(int idade) throws IdadeInvalidaException {
+        if (idade < 18) {
+            throw new IdadeInvalidaException("A idade mínima é 18 anos.");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            verificarIdade(15);
+        } catch (IdadeInvalidaException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
