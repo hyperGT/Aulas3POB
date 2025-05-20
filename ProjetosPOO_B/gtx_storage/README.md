a74eaec0e87d6acf1b0cd405164e8a8bc0ba7fb7
hyperGT <charlesgabriel102@gmail.com>
2025-05-20 18:21:59 -0300

    New Feats Dois métodos adicionados na classe arquivo: fileExists e readFile.

--- ProjetosPOO_B/gtx_storage/README.md
+++ ProjetosPOO_B/gtx_storage/README.md
@@ -1,7 +1,3 @@
-## Getting Started
-
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
-
 ## Folder Structure
 
 The workspace contains two folders by default, where:
@@ -13,6 +9,19 @@ Meanwhile, the compiled output files will be generated in the `bin` folder by de
 
 > If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
 
-## Dependency Management
+# What I've Learned
 
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+## File Handling
+
+- How to read a file
+      There are few options that can be useful if you want read a file:
+    - BufferedReader
+
+
+- How to write a file
+    the most popular options:
+  - FileWriter
+  - BufferedWriter
+  - PrintWriter
+  - FileOutputStream
+
--- ProjetosPOO_B/gtx_storage/src/util/Arquivo.java
+++ ProjetosPOO_B/gtx_storage/src/util/Arquivo.java
@@ -1,12 +1,39 @@
 package util;
 
-import java.io.File;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Arquivo {
 
-    public void existe(String filePath) {
+    // variables
+    private String path;
+
+    // constructor
+    public Arquivo (String path){
+        this.path = path;
+    }
+
+    // methods
+    public void readFile(){
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
+
+            String line;
+
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Could not locale file");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void fileExists(String file) {
         
         System.out.println("Diretório de busca atual: " + System.getProperty("user.dir"));
         
@@ -17,9 +44,25 @@ public class Arquivo {
         }else{
             System.out.println("Arquivo não encontrado, verifique se o diretório buscado está correto.");
         }
-    }    
+    }
 
+    public void writeFile(){
+
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    // teste
     public static void main(String[] args) {
 
+        Arquivo arq = new Arquivo("src/util/dados.txt");
+
+        arq.readFile();
     }
 }
